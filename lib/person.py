0b9fd1a98892d2df9564ffbedc22c42427dbe5e8
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-26 20:51:55 +0300

    passes

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+
+    def walk(self):
+        print("The person is walking.")
+
+#self.walk() #This will cause an infinite loop
+guido=Person()
+guido.talk()
+guido.walk()
